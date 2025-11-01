d3d9c064bbdfee6654dbe20090aa7ebf99ddb5b6
danubiobwm <danubio.bwm@gmail.com>
2025-11-01 16:40:06 -0300

    feat: colaborador_repository

--- internal/repositories/colaborador_repository.go
+++ internal/repositories/colaborador_repository.go
@@ -1,4 +1,51 @@
 package repositories
 
-func colaborador_repository() {
+import (
+	"errors"
+
+	"github.com/danubiobwm/company-api/internal/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type ColaboradorRepository struct {
+	db *gorm.DB
+}
+
+func NewColaboradorRepository(db *gorm.DB) *ColaboradorRepository {
+	return &ColaboradorRepository{db: db}
+}
+
+func (r *ColaboradorRepository) Create(c *models.Colaborador) error {
+	return r.db.Create(c).Error
+}
+
+func (r *ColaboradorRepository) GetByID(id uuid.UUID) (*models.Colaborador, error) {
+	var c models.Colaborador
+	if err := r.db.First(&c, "id = ?", id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &c, nil
+}
+
+func (r *ColaboradorRepository) GetByCPF(cpf string) (*models.Colaborador, error) {
+	var c models.Colaborador
+	if err := r.db.First(&c, "cpf = ?", cpf).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &c, nil
+}
+
+func (r *ColaboradorRepository) Update(c *models.Colaborador) error {
+	return r.db.Save(c).Error
+}
+
+func (r *ColaboradorRepository) Delete(id uuid.UUID) error {
+	return r.db.Delete(&models.Colaborador{}, "id = ?", id).Error
 }
